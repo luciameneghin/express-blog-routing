3508a2de01564c7e41e1b74ab310a15003d40b13
luciameneghin <lucia.meneghin00@gmail.com>
2024-12-18 13:43:50 +0100

    feat: root CRUD created

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const postsObj = require('./data/posts')
 const postsRouter = require('./routers/posts')
 
 
+app.get('/', (req, res) => {
+  res.send('server dei posts')
+})
+
+app.use('/posts', postsRouter)
+
 app.listen(port, () => {
-  console.log(`port: ${port}`)
+  console.log(`porta: ${port}`)
 })
--- routers/posts.js
+++ routers/posts.js
@@ -1,2 +1,34 @@
 const express = require('express');
 const router = express.Router();
+
+//index
+router.get('/', (req, res) => {
+  res.send('Lista di tutti i post')
+})
+
+//show
+router.get('/:id', (req, res) => {
+  res.send(`dettagli posts con id ${req.params.id}`)
+})
+
+//store
+router.post('/', (req, res) => {
+  res.send('Creazione nuovo post')
+})
+
+//update
+router.put('/:id', (req, res) => {
+  res.send(`Modifico il post con id ${req.params.id}`)
+})
+
+//modify
+router.patch('/:id', (req, res) => {
+  res.send(`Modifico solo in parte il post con id ${req.params.id}`)
+})
+
+//destroy
+router.delete('/:id', (req, res) => {
+  res.send(`Cancello il post con id ${req.params.id}`)
+})
+
+module.exports = router
